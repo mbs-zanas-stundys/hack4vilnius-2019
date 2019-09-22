b975ae2f41683c6cb6fbd7e9c8fd21fcfd5c3b80
Donatas Zubavicius <donatas.zubavicius@metasite.net>
2019-09-22 11:33:21 +0300

    Add low ratio containe

--- api/src/main/java/lt/metasite/waste/container/repository/ScheduleRepositoryImpl.java
+++ api/src/main/java/lt/metasite/waste/container/repository/ScheduleRepositoryImpl.java
@@ -1,13 +1,24 @@
 package lt.metasite.waste.container.repository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import lt.metasite.waste.container.Schedule;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+
+import static com.mongodb.client.model.Filters.where;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
+import static org.springframework.data.mongodb.core.aggregation.ComparisonOperators.valueOf;
 
 public class ScheduleRepositoryImpl implements ScheduleRepositoryCustom {
 
@@ -19,8 +30,21 @@ public class ScheduleRepositoryImpl implements ScheduleRepositoryCustom {
 
     @Override
     public void setNearestScheduleToCompleted(LocalDateTime date, String containerNo) {
-        mongoTemplate.updateFirst(new Query(new Criteria("actualDate").is(null).and("expectedDate").lte(date.toLocalDate())),
+        mongoTemplate.updateFirst(new Query(new Criteria("actualDate").exists(false).and(
+                "expectedDate").lte(date.toLocalDate())),
                                   Update.update("actualDate",date),
                                   Schedule.class);
+    }
+
+    @Override
+    public List<Schedule> getDelayedSchedules(LocalDate dateFrom, LocalDate dateTo) {
+
+        List<AggregationOperation> operations = new ArrayList<>();
+        operations.add(match(new Criteria("actualDate").exists(true)));
+        TypedAggregation<Schedule> aggregation =
+                newAggregation(Schedule.class,
+                               operations
+                );
+        return mongoTemplate.aggregate(aggregation, Schedule.class).getMappedResults();
     }
 }
