038006233a7cdc7231bf11a8dbe07ab70f1bbc92
Žanas Stundys <zanas.stundys@metasite.net>
2019-10-25 21:35:42 +0300

    Fix swagger

--- api/src/main/java/lt/vilnius/waste/config/SwaggerConfig.java
+++ api/src/main/java/lt/vilnius/waste/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("lt.metasite.waste"))
+                .apis(RequestHandlerSelectors.basePackage("lt.vilnius.waste"))
                 .paths(PathSelectors.any())
                 .build();
     }
